00ccf1d797a9ac6be1c39bf3b1e7082d5ce0fcee
strinsberg <stevendkom@gmail.com>
2020-06-25 21:19:44 -0600

    Setting up the app to be packeable.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='simple-text',
+    version='0.1.0',
+    packages=['dgsl_engine'],
+    python_requires='>=3',
+    description='A simple toy terminal text editor',
+    url='https://github.com/strinsberg/simple-text',
+    author='Steven Deutekom',
+    entry_points = {
+        'console_scripts': ['simple-text=simple-text.__main__:main'],
+    }
+)
